766f40d296615acf3aa2911bd61c3a5a03742007
RandyYanish <rtaylorfargo@gmail.com>
2023-04-30 22:18:30 -0500

    setup for questions and generating markdown

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,92 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "Apache 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "GPL 3.0":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "BSD 3":
+      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    default:
+      return "";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that returns the license link
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "Apache 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GPL 3.0":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+    case "BSD 3":
+      return "https://opensource.org/licenses/BSD-3-Clause";
+    default:
+      return "";
+  }
+}
+
+// Function that returns the license section of README
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+
+  return `
+## License\n
+
+This project is licensed under the ${license} license. See [LICENSE](${renderLicenseLink(license)}) for details.
+`;
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title}\n
+  ${renderLicenseBadge}\n
 
+## Description\n
+
+${data.description}\n
+
+### Table of Contents:\n
+
+[Installation](#installation)\n
+[Usage](#usage)\n
+[License](#license)\n
+[Contributing](#contributing)\n
+[Tests](#tests)\n
+[Questions](#questions)\n
+
+## Installation\n
+
+${data.installation}\n
+
+## Usage\n
+
+${data.usage}\n
+
+${renderLicenseSection(license)}\n
+
+## Contributing\n
+
+## Tests\n
+
+## Questions?\n
+
+${data.username}\n
 `;
 }
 
--- index.mjs
+++ index.mjs
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs";
+import generateMarkdown from "./generateMarkdown";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the Title of your project?', 'Write a short description about what your project is about:', 'How can your users install this project and use it?', 'What will this application be used for?', 'What are your guidelines for contributing to this project?', 'How can users test this project?', 'What license are you going to use?', 'What is your Github username?', 'What is your email address?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer    
+        .prompt(
+            {
+                type: 'input',
+                message: questions[0],
+                name: 'title'
+            },
+            {
+                type: 'input',
+                message: questions[1],
+                name: 'description'
+            },
+            {
+                type: 'input',
+                message: questions[2],
+                name: 'installation'
+            },
+            {
+                type: 'input',
+                message: questions[3],
+                name: 'usage'
+            },
+            {
+                type: 'input',
+                message: questions[4],
+                name: 'contribution'
+            },
+            {
+                type: 'input',
+                message: questions[5],
+                name: 'test'
+            },
+            {
+                type: 'list',
+                message: questions[6],
+                choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', ''],
+                name: 'license'
+            },
+            {
+                type: 'input',
+                message: questions[7],
+                name: 'username'
+            },
+            {
+                type: 'input',
+                message: questions[8],
+                name: 'email'
+            },
+        )
+        // TODO: fix responses and format to write file
+        .then(responses) {
+            responses = data;
+            writeToFile(responses.title, data);
+        };
+}
 
 // Function call to initialize app
 init();
